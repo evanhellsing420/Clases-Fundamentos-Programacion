6880dc7dac6a6e2513cfb69607dffaa0a65328b2
evanhellsing420 <marianleonpaez10@gmail.com>
2026-03-04 16:50:54 -0600

    practica_06

---
+++ Clase_06/main.js
@@ -0,0 +1,28 @@
+
+
+// numeros +, -, *, /, >, <
+
+//const suma= 3 * ((40 + 20) * 2);
+//console.log(suma);
+
+// strings
+//let firstName = 'Marian';
+//const lastName = 'Leon';
+
+//console.log(firstName + lastName);
+//console.error('ESTO ES UN ERROR!!');
+
+// booleans
+
+//true || false 
+
+const num = 10;
+//console.log (num > 2);
+
+// && --> and o y
+console.log (num == 10 && num < 2);
+
+// || --> or / o
+console.log (num == 10 || num < 2);
+
+
