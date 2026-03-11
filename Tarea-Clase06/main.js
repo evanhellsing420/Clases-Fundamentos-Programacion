021527cc6901d73413324220b01eb5bcb25876e1
evanhellsing420 <marianleonpaez10@gmail.com>
2026-03-10 23:56:38 -0600

    Tarea

---
+++ Tarea-Clase06/main.js
@@ -0,0 +1,3 @@
+const number = 35;
+const result = (number >= 10) && (number <= 20) || (number >= 40) && (number <= 60);
+console.log('El cálculo de rangos para el número ' + number + ' es: ' + result);
